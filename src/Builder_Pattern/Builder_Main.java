c9e3edc46c3f9499d3b46250c70da82cfbafc989
donghyuk0605 <donghyuk65@gmail.com>
2021-12-11 18:38:09 +0900

    Builder_PatternV4

--- src/Builder_Pattern/Builder_Main.java
+++ src/Builder_Pattern/Builder_Main.java
@@ -6,10 +6,13 @@ public class Builder_Main {
 		// TODO Auto-generated method stub
 		Coffee iceAmericano = new Coffee.CoffeeBuilder("아이스아메리카노", 2000)
 				.setICE_YN(true)
+				//펄스를 명시를 해도되고 안해도 된다 선택사항
 				.setMILK_YN(false)
 				.build();
 		Coffee hotCafelatte= new Coffee.CoffeeBuilder("카페라떼", 3000)
+				//아이스의 경우 기본값이 펄스라서 명시를안해도 자동으로 펄스가된다
 				.setMILK_YN(true)
+				
 				.build();
 		System.out.println(iceAmericano.getName()+iceAmericano.getPrice()+iceAmericano.isICE_YN()+iceAmericano.isMILK_YN());
 		System.out.println(hotCafelatte.getName()+hotCafelatte.getPrice()+hotCafelatte.isICE_YN()+hotCafelatte.isMILK_YN());
