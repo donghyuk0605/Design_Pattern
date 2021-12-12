9009549a703b603c99dce804cb9dec7d3ed0c458
donghyuk0605 <donghyuk65@gmail.com>
2021-12-12 21:42:41 +0900

    Adapter_PatternV3

---
+++ src/Adapter_Pattern/ALZip.java
@@ -0,0 +1,5 @@
+package Adapter_Pattern;
+
+public interface ALZip {
+	void unalzip(String filefomat);
+}
---
+++ src/Adapter_Pattern/WinZip.java
@@ -0,0 +1,5 @@
+package Adapter_Pattern;
+
+public interface WinZip {
+	void unzip(String filefomat);
+}
