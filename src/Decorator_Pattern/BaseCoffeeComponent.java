021f6f7817c339b6c27a7baefc9e125305d6b192
donghyuk0605 <donghyuk65@gmail.com>
2021-12-12 21:46:26 +0900

    Decorator_PatternV1

---
+++ src/Decorator_Pattern/BaseCoffeeComponent.java
@@ -0,0 +1,11 @@
+package Decorator;
+
+public class BaseCoffeeComponent implements CoffeeCompomemt {
+
+	@Override
+	public String Ingredients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
