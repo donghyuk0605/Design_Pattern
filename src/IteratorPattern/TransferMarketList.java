812785704345df0b415206d8d6dadaa0f83cbfec
donghyuk0605 <donghyuk65@gmail.com>
2021-12-21 14:06:48 +0900

    Iterator_PatternV5

--- src/IteratorPattern/TransferMarketList.java
+++ src/IteratorPattern/TransferMarketList.java
@@ -6,15 +6,18 @@ public class TransferMarketList implements Aggregate {
 
 	public TransferMarketList(int size) {
 		soccerPlayers = new SoccerPlayer[size];
+		//아적시장 (명단)배열에 사이즈 지정
 	}
 
 	public SoccerPlayer getSoccerPlayer(int index) {
 		return soccerPlayers[index];
+		//값을 가져오기
 	}
 
 	public void addSoccerPlayer(SoccerPlayer soccerPlayer) {
 		soccerPlayers[size] = soccerPlayer;
 		size++;
+		//값 삽입하기 
 
 	}
 
